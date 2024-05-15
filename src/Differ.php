2b50776cf65e090824860522987993729372dbac
Axel <axel19911@gmail.com>
2024-05-15 14:55:17 +0300

    refactoring src/Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -2,6 +2,37 @@
 
 namespace Differ\Differ;
 
+function genDiff(string $firstFilePath, string $secondFilePath): string
+{
+    $file1 = json_decode(file_get_contents($firstFilePath), true);
+    $file2 = json_decode(file_get_contents($secondFilePath), true);
+    $result = implode("\n" ,array_map(fn($item) => "  $item", combine($file1, $file2)));
+
+    return "\n{\n" . $result . "\n}\n";
+}
+
+function combine(array $json1, array $json2): array
+{
+    $sorted = sorting(
+        array_merge(
+        setSign(array_diff($json1, $json2), '-'),
+        setSign(array_diff($json2, $json1), '+'), 
+        setSign(array_intersect($json1, $json2))
+    ));
+    
+    return array_map(function ($item) {
+        [$key, $val, $sign] = $item;
+        return empty($sign) ? "  $key: $val" : "$sign $key: $val";
+    }, $sorted);
+}
+
+function sorting(array $arr): array
+{
+    usort($arr, fn($a, $b) => $a[0] <=> $b[0]);
+
+    return $arr;
+}
+
 function setSign(array $arr, string $sign = ''): array
 {
     return array_map(function ($key, $val) use ($sign) {
@@ -14,26 +45,4 @@ function setSign(array $arr, string $sign = ''): array
     }, array_keys($arr), $arr);
 }
 
-function combine(array $json1, array $json2): array
-{
-    $merged = array_merge(
-        setSign(array_diff($json1, $json2), '-'),
-        setSign(array_diff($json2, $json1), '+'), 
-        setSign(array_intersect($json1, $json2))
-    );
-    usort($merged, fn($a, $b) => $a[0] <=> $b[0]);   
-    
-    return array_map(function ($item) {
-        [$key, $val, $sign] = $item;
-        return empty($sign) ? "  $key: $val" : "$sign $key: $val";
-    }, $merged);
-}
 
-function genDiff(string $firstFilePath, string $secondFilePath): string
-{
-    $file1 = json_decode(file_get_contents($firstFilePath), true);
-    $file2 = json_decode(file_get_contents($secondFilePath), true);
-    $result = implode("\n" ,array_map(fn($item) => "  $item", combine($file1, $file2)));
-
-    return "\n{\n" . $result . "\n}\n";
-}
