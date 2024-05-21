0bbd6c3907b2a2e43a2fe19765db8445b91ce79b
Axel <axel19911@gmail.com>
2024-05-21 17:46:07 +0300

    refactoring Differ.php & Parsers.php

--- src/Differ.php
+++ src/Differ.php
@@ -16,37 +16,3 @@ function genDiff(string $firstFilePath, string $secondFilePath): string
     return "\n{\n" . $result . "\n}\n";
 }
 
-function combine(array $file1, array $file2): array
-{
-    $sorted = sorting(
-        array_merge(
-            setSign(array_diff($file1, $file2), '-'),
-            setSign(array_diff($file2, $file1), '+'),
-            setSign(array_intersect($file1, $file2))
-        )
-    );
-
-    return array_map(function ($item) {
-        [$key, $val, $sign] = $item;
-        return empty($sign) ? "  $key: $val" : "$sign $key: $val";
-    }, $sorted);
-}
-
-function sorting(array $arr): array
-{
-    usort($arr, fn($a, $b) => $a[0] <=> $b[0]);
-
-    return $arr;
-}
-
-function setSign(array $arr, string $sign = ''): array
-{
-    return array_map(function ($key, $val) use ($sign) {
-        if ($val === true) {
-            $val = 'true';
-        } elseif ($val === false) {
-            $val = 'false';
-        }
-        return [$key, $val, $sign];
-    }, array_keys($arr), $arr);
-}
--- src/Parsers.php
+++ src/Parsers.php
@@ -4,12 +4,45 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-use function Differ\Differ\combine;
-
 function decoder($firstFilePath, $secondFilePath)
 {
     $file1 = json_decode(file_get_contents($firstFilePath), true);
     $file2 = json_decode(file_get_contents($secondFilePath), true);
     $result = implode("\n", array_map(fn($item) => "  $item", combine($file1, $file2)));
     return $result;
+}
+
+function combine(array $file1, array $file2): array
+{
+    $sorted = sorting(
+        array_merge(
+            setSign(array_diff($file1, $file2), '-'),
+            setSign(array_diff($file2, $file1), '+'),
+            setSign(array_intersect($file1, $file2))
+        )
+    );
+
+    return array_map(function ($item) {
+        [$key, $val, $sign] = $item;
+        return empty($sign) ? "  $key: $val" : "$sign $key: $val";
+    }, $sorted);
+}
+
+function sorting(array $arr): array
+{
+    usort($arr, fn($a, $b) => $a[0] <=> $b[0]);
+
+    return $arr;
+}
+
+function setSign(array $arr, string $sign = ''): array
+{
+    return array_map(function ($key, $val) use ($sign) {
+        if ($val === true) {
+            $val = 'true';
+        } elseif ($val === false) {
+            $val = 'false';
+        }
+        return [$key, $val, $sign];
+    }, array_keys($arr), $arr);
 }
